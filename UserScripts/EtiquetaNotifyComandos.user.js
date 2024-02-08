70fa1411fd9bcae174585d0909c932b2a9b0d909
João Paulo <65697347+p4ulinho@users.noreply.github.com>
2024-02-08 05:44:19 -0300

    Create EtiquetaNotifyComandos.user.js

---
+++ UserScripts/EtiquetaNotifyComandos.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         Etiquetador Comandos + Notificar no Telegram
+// @include      https://**mode=incomings*
+// @author       PaulinhoTutoriais
+// @icon         https://www.dl.dropboxusercontent.com/scl/fi/93px3rbj21zx3863m0g40/letter-p.png?rlkey=yyoejfwogbl9rdw0p2fmft8lr&dl=0
+// ==/UserScript==
+
+token = "SEU TOKEN AQUI"
+
+$.ajax({
+  url: 'https://paulinhoscripts.com.br/sendNotifyWhatsapp.js',
+  dataType: 'script',
+  cache: true,
+});
